6a5d452875d7226401c6f0a7becb53c848497fb7
ferreira-mev <ferreira.mev@gmail.com>
2021-10-11 10:20:42 -0300

    Add missing .hooks and  base url

--- scraper/polite-requests.py
+++ scraper/polite-requests.py
@@ -1,11 +1,25 @@
-import requests
+"""
+Creates custom Session objects for usage in the scraper,
+compliant with the Wikimedia User-Agent policy, as given in
+https://meta.wikimedia.org/wiki/User-Agent_policy
 
-# See https://docs.python-requests.org/en/master/user/advanced/#session-objects
-# and https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/
+Refs:
+https://docs.python-requests.org/en/master/user/advanced/#session-objects
+https://toolbelt.readthedocs.io/en/latest/sessions.html#baseurlsession
+https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/
+"""
+
+import requests
+from requests_toolbelt import sessions
+
+wiki_root = "https://en.wikipedia.org"
+# no trailing / to make string concatenation simpler
+# also no /wiki because that's included in scraped links
 
 def raise_by_default(response, *args, **kwargs):
     response.raise_for_status()
 
-sesh = requests.Session()
+sesh = sessions.BaseUrlSession(base_url=wiki_root)
 
-sesh["response"].append(raise_by_default)
+sesh.hooks["response"].append(raise_by_default)
+
