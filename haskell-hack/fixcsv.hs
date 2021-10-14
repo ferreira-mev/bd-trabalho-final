0b44078f9c88acedc6cb2230c25edf3e4168d9e7
Hashi364 <49736221+Kiyoshi364@users.noreply.github.com>
2021-10-14 20:33:58 -0300

    Normaziling csv's
        Stringfying String Columns
        Translating Header

---
+++ haskell-hack/fixcsv.hs
@@ -0,0 +1,36 @@
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    args     <- getArgs
+    filename <- return $ head args
+    col      <- return $ tail args
+    newfname <- return $ filename ++ "-mod"
+    file     <- fmap (while $ map (stringfy . read) col) $ readFile filename
+    writeFile newfname file
+
+columns :: Char -> String -> [String]
+columns c []  = []
+columns c str = (takeW str) : (columns c $ drop 1 $ dropW str)
+    where takeW = takeWhile (/=c)
+          dropW = dropWhile (/=c)
+
+uncolumns :: Char -> [String] -> String
+uncolumns c = foldr1 (\l r -> l ++ [c] ++ r)
+
+stringfy :: Int -> String -> String
+stringfy col csv = unlines $ header ++ newlines
+    where header = take 1 $ lines csv
+          linhas = tail $ lines csv
+          modLine = uncolumns ',' . at col addQuotes . columns ','
+          newlines = map modLine linhas
+          addQuotes s = if s == "null" then s else "'" ++ s ++ "'"
+
+at :: Int -> (a -> a) -> [a] -> [a]
+at _ _  []      = []
+at n f (x:xs)
+    | n == 0    = (f x) : xs
+    | otherwise = x : at (n-1) f xs
+
+while :: [a -> a] -> a -> a
+while = foldr1 (.)
---
+++ haskell-hack/structure.sql
@@ -0,0 +1,135 @@
+CREATE DATABASE IF NOT EXISTS stackoverflow
+
+USE stackoverflow;
+
+CREATE TABLE Pessoa (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Genero ENUM,
+    FaixaEtaria ENUM,
+    ExpTotal VARCHAR(255),
+    ExpProfissional VARCHAR(255),
+    EhProfissional ENUM,
+    TamEmpresa ENUM,
+    Cargo ENUM,
+    Salario INT,
+    NivelEduc ENUM,
+    Pais VARCHAR(255)
+);
+
+CREATE TABLE Sgbd (
+    Relacional BOOL,
+    Logo VARCHAR(511),
+    Nome VARCHAR(255),
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    AnoCriacao YEAR,
+    UNIQUE (Logo, Nome)
+);
+
+CREATE TABLE Linguagem (
+    Logo VARCHAR(511),
+    Nome VARCHAR(255) NOT NULL,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    AnoCriacao YEAR,
+    UNIQUE (Logo, Nome)
+);
+
+CREATE TABLE OutraTecnologia (
+    Tipo ENUM('Biblioteca', 'Cloud', 'Framework Web', 'IDE', 'SO', 'Outras Tecnologias') NOT NULL,
+    Logo VARCHAR(511),
+    Nome VARCHAR(255),
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    AnoCriacao YEAR,
+    UNIQUE (Logo, Nome)
+);
+
+CREATE TABLE Paradigma (
+    Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Usa (
+    fk_Pessoa_Id INT NOT NULL,
+    fk_Sgbd_Id INT,
+    fk_Linguagem_Id INT,
+    fk_OutraTecnologia_Id INT,
+    Id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Deseja (
+    fk_Pessoa_Id INT NOT NULL,
+    fk_Sgbd_Id INT,
+    fk_Linguagem_Id INT,
+    fk_OutraTecnologia_Id INT,
+    Id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Associada (
+    fk_Linguagem_Id INT NOT NULL,
+    fk_OutraTecnologia_Id INT NOT NULL,
+    Dependente BOOL,
+    Id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Tem (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_Linguagem_Id INT NOT NULL,
+    fk_Paradigma_Id INT NOT NULL
+);
+
+ALTER TABLE Usa ADD CONSTRAINT FK_Usa_1
+    FOREIGN KEY (fk_Pessoa_Id)
+    REFERENCES Pessoa (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Usa ADD CONSTRAINT FK_Usa_2
+    FOREIGN KEY (fk_Sgbd_Id)
+    REFERENCES Sgbd (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Usa ADD CONSTRAINT FK_Usa_3
+    FOREIGN KEY (fk_Linguagem_Id)
+    REFERENCES Linguagem (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Usa ADD CONSTRAINT FK_Usa_4
+    FOREIGN KEY (fk_OutraTecnologia_Id)
+    REFERENCES OutraTecnologia (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_1
+    FOREIGN KEY (fk_Pessoa_Id)
+    REFERENCES Pessoa (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_2
+    FOREIGN KEY (fk_Sgbd_Id)
+    REFERENCES Sgbd (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_3
+    FOREIGN KEY (fk_Linguagem_Id)
+    REFERENCES Linguagem (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_4
+    FOREIGN KEY (fk_OutraTecnologia_Id)
+    REFERENCES OutraTecnologia (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Associada ADD CONSTRAINT FK_Associada_1
+    FOREIGN KEY (fk_Linguagem_Id)
+    REFERENCES Linguagem (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Associada ADD CONSTRAINT FK_Associada_2
+    FOREIGN KEY (fk_OutraTecnologia_Id)
+    REFERENCES OutraTecnologia (Id)
+    ON DELETE SET NULL;
+
+ALTER TABLE Tem ADD CONSTRAINT FK_Tem_2
+    FOREIGN KEY (fk_Linguagem_Id)
+    REFERENCES Linguagem (Id);
+
+ALTER TABLE Tem ADD CONSTRAINT FK_Tem_3
+    FOREIGN KEY (fk_Paradigma_Id)
+    REFERENCES Paradigma (Id);
