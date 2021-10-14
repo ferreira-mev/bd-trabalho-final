255790460cc0cd024591d08e2a1b753719502e6d
ferreira-mev <ferreira.mev@gmail.com>
2021-10-14 20:03:23 -0300

    (Adicoes do Hash no Discord)

--- sql/ddl_brmodelo.sql
+++ sql/ddl_brmodelo.sql
@@ -4,8 +4,12 @@ TODO:
 - Adc restr integridade nao desejar SO?
 */
 
+CREATE DATABASE IF NOT EXISTS stackoverflow
+
+USE stackoverflow;
+
 CREATE TABLE Pessoa (
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     Genero ENUM,
     FaixaEtaria ENUM,
     ExpTotal VARCHAR(255),
@@ -19,10 +23,10 @@ CREATE TABLE Pessoa (
 );
 
 CREATE TABLE Sgbd (
-    Relacional BIT(1),
+    Relacional BOOL,
     Logo VARCHAR(511),
     Nome VARCHAR(255),
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     AnoCriacao YEAR,
     UNIQUE (Logo, Nome)
 );
@@ -30,22 +34,22 @@ CREATE TABLE Sgbd (
 CREATE TABLE Linguagem (
     Logo VARCHAR(511),
     Nome VARCHAR(255) NOT NULL,
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     AnoCriacao YEAR,
     UNIQUE (Logo, Nome)
 );
 
 CREATE TABLE OutraTecnologia (
-    Tipo ENUM NOT NULL,
+    Tipo ENUM('Biblioteca', 'Cloud', 'Framework Web', 'IDE', 'SO', 'Outras Tecnologias') NOT NULL,
     Logo VARCHAR(511),
     Nome VARCHAR(255),
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     AnoCriacao YEAR,
     UNIQUE (Logo, Nome)
 );
 
 CREATE TABLE Paradigma (
-    Id INT NOT NULL PRIMARY KEY,
+    Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(255) NOT NULL UNIQUE
 );
 
@@ -54,7 +58,7 @@ CREATE TABLE Usa (
     fk_Sgbd_Id INT,
     fk_Linguagem_Id INT,
     fk_OutraTecnologia_Id INT,
-    Id INT PRIMARY KEY
+    Id INT PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE Deseja (
@@ -62,76 +66,76 @@ CREATE TABLE Deseja (
     fk_Sgbd_Id INT,
     fk_Linguagem_Id INT,
     fk_OutraTecnologia_Id INT,
-    Id INT PRIMARY KEY
+    Id INT PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE Associada (
     fk_Linguagem_Id INT NOT NULL,
     fk_OutraTecnologia_Id INT NOT NULL,
-    Dependente BIT(1),
-    Id INT PRIMARY KEY
+    Dependente BOOL,
+    Id INT PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE Tem (
-    Id INT PRIMARY KEY,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
     fk_Linguagem_Id INT NOT NULL,
     fk_Paradigma_Id INT NOT NULL
 );
- 
+
 ALTER TABLE Usa ADD CONSTRAINT FK_Usa_1
     FOREIGN KEY (fk_Pessoa_Id)
     REFERENCES Pessoa (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Usa ADD CONSTRAINT FK_Usa_2
     FOREIGN KEY (fk_Sgbd_Id)
     REFERENCES Sgbd (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Usa ADD CONSTRAINT FK_Usa_3
     FOREIGN KEY (fk_Linguagem_Id)
     REFERENCES Linguagem (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Usa ADD CONSTRAINT FK_Usa_4
     FOREIGN KEY (fk_OutraTecnologia_Id)
     REFERENCES OutraTecnologia (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_1
     FOREIGN KEY (fk_Pessoa_Id)
     REFERENCES Pessoa (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_2
     FOREIGN KEY (fk_Sgbd_Id)
     REFERENCES Sgbd (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_3
     FOREIGN KEY (fk_Linguagem_Id)
     REFERENCES Linguagem (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_4
     FOREIGN KEY (fk_OutraTecnologia_Id)
     REFERENCES OutraTecnologia (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Associada ADD CONSTRAINT FK_Associada_1
     FOREIGN KEY (fk_Linguagem_Id)
     REFERENCES Linguagem (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Associada ADD CONSTRAINT FK_Associada_2
     FOREIGN KEY (fk_OutraTecnologia_Id)
     REFERENCES OutraTecnologia (Id)
     ON DELETE SET NULL;
- 
+
 ALTER TABLE Tem ADD CONSTRAINT FK_Tem_2
     FOREIGN KEY (fk_Linguagem_Id)
     REFERENCES Linguagem (Id);
- 
+
 ALTER TABLE Tem ADD CONSTRAINT FK_Tem_3
     FOREIGN KEY (fk_Paradigma_Id)
     REFERENCES Paradigma (Id);
