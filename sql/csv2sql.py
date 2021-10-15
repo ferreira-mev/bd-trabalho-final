99a6de0eb57127494599d2c6ad391e7959888ba7
ferreira-mev <ferreira.mev@gmail.com>
2021-10-15 08:09:00 -0300

    Adc teste da conv csv-SQL apenas com Sgbd

---
+++ sql/csv2sql.py
@@ -0,0 +1,48 @@
+"""
+Script para gerar a DML em SQL a partir dos arquivos csv.
+
+A parte DDL já foi gerada semiautomaticamente a partir da
+conversão do modelo lógico feito no brModelo.
+"""
+import csv
+
+csv_path = "../datasets/"
+output_path = "dml/"
+
+enum_types = ('Biblioteca', 'Nuvem', 'FrameworkWeb', 'EditorIde', 'So', 'Outro')
+# Já num formato que dá para copiar e colar da/na DDL caso alteremos
+# (Obs: melhor deixar com aspas simples)
+
+csv_types = [
+    "cloud",
+    "databases",
+    "editors-ides",
+    "languages",
+    "libs",
+    "oss",
+    "other-tech",
+    "web-frameworks"
+    ]
+
+type_dict = {c: e for c, e in zip(csv_types, enum_types)}
+# "Tradução" entre nome conforme csv e conforme a enum
+# (Obs: Atenção à ordem se forem alterar)
+
+# Teste apenas com os Sgbds:
+
+with open("../datasets/databases.csv", "r") as csv_file:
+    with open(output_path + "test-output.sql", "w") as out_file:
+        fields = csv_file.readline().replace("\r","").replace("\n","")
+        for row in csv_file:
+            insert = "INSERT INTO Sgbd(" + fields + ")\n"
+            insert += "VALUES ("
+            insert += row.replace("\r","").replace("\n","")
+            insert += ");\n"
+
+            out_file.write(insert)
+
+            
+# TODO/Lembretes:
+# - construir entidades para os paradigmas
+# - incluir o tipo como uma coluna extra (renomear os arquivos?)
+# - processar associations.csv
