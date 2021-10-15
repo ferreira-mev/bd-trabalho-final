9e63f8c129019e4b45b60990cca957026ede85b2
ferreira-mev <ferreira.mev@gmail.com>
2021-10-15 09:55:43 -0300

    Add DML file to repo

---
+++ sql/dml.sql
@@ -0,0 +1,468 @@
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('AWS',2002,'AWS.png','Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('DigitalOcean',2011,'DigitalOcean.png','Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Google Cloud Platform',2008,'Google_Cloud_Platform.png','Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Heroku',2007,'Heroku.png','Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('IBM Cloud or Watson',null,'IBM_Cloud_or_Watson.png','Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Microsoft Azure',2008,'Microsoft_Azure.png','Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Oracle Cloud Infrastructure',null,null,'Nuvem');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Android Studio',null,'Android_Studio.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Atom',2014,'Atom.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Eclipse',2001,'Eclipse.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Emacs',1976,'Emacs.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('IntelliJ',2001,'IntelliJ.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('IPython/Jupyter',2015,'Jupyter.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('NetBeans',null,'NetBeans.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Neovim',1991,'Neovim.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Notepad++',2003,'Notepadpp.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('PHPStorm',2009,'PHPStorm.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('PyCharm',2010,'PyCharm.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Rider',null,'Rider.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('RStudio',2011,'RStudio.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('RubyMine',null,'RubyMine.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Sublime Text',2008,'Sublime_Text.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('TextMate',2004,'TextMate.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Vim',1991,'Vim.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Visual Studio',null,'Visual_Studio.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Visual Studio Code',2015,'Visual_Studio_Code.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Webstorm',null,'Webstorm.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Xcode',null,'Xcode.png','EditorIde');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('.NET Framework',2002,'dotNET_Framework.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('.NET Core / .NET 5',2016,'dotNET.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Apache Spark',2014,'Apache_Spark.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Cordova',2009,'Cordova.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Flutter',2017,'Flutter.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Hadoop',2006,'Hadoop.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Keras',2015,'Keras.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('NumPy',1995,'NumPy.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Pandas',2008,'Pandas.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Qt',1995,'Qt.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('React Native',2015,'React_Native.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('TensorFlow',2015,'TensorFlow.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Torch/PyTorch',2002,'Torch.png','Biblioteca');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('BSD',1977,null,'So');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Linux-based',1991,'Linux-based.png','So');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('MacOS',2001,'MacOS.png','So');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Windows',1985,'Windows.png','So');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Windows Subsystem for Linux (WSL)',2016,'Windows_Subsystem_for_Linux_(WSL).png','So');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Ansible',2012,'Ansible.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Chef',2009,'Chef.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Deno',2018,'Deno.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Docker',2013,'Docker.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Flow',null,null,'Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Git',2005,'Git.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Kubernetes',2014,'Kubernetes.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Pulumi',null,null,'Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Puppet',2005,'Puppet.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Terraform',2014,'Terraform.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Unity 3D',null,'Unity_3D.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Unreal Engine',null,'Unreal_Engine.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Xamarin',null,'Xamarin.png','Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Yarn',null,null,'Outro');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Angular',2010,'Angular.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Angular.js',2010,'Angularjs.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('ASP.NET',2002,null,'FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('ASP.NET Core',null,null,'FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Django',2005,'Django.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Drupal',2001,'Drupal.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Express',2010,'Express.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('FastAPI',2018,null,'FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Flask',2010,'Flask.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Gatsby',null,null,'FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('jQuery',2006,'jQuery.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Laravel',2011,'Laravel.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('React.js',2013,'Reactjs.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Ruby on Rails',2004,'Ruby_on_Rails.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Spring',2002,'Spring.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Svelte',2016,'Svelte.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Symfony',2005,'Symfony.png','FrameworkWeb');
+
+INSERT INTO OutraTecnologia(Nome,AnoCriacao,Logo,Tipo)
+VALUES ('Vue.js',2014,'Vuejs.png','FrameworkWeb');
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Cassandra',2008,'Cassandra.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Couchbase',2010,'Couchbase.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('DynamoDB',2012,'DynamoDB.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Elasticsearch',2010,'Elasticsearch.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Firebase',2012,'Firebase.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('IBM DB2',1993,'IBM_DB2.png',True);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('MariaDB',2009,'MariaDB.png',True);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Microsoft SQL Server',1989,null,True);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('MongoDB',2009,'MongoDB.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('MySQL',1995,'MySQL.png',True);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Oracle',1979,'Oracle.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('PostgreSQL',1996,'PostgreSQL.png',True);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('Redis',2009,'Redis.png',False);
+
+INSERT INTO Sgbd(Nome,AnoCriacao,Logo,Relacional)
+VALUES ('SQLite',2000,'SQLite.png',True);
+
+INSERT INTO Paradigma(Nome)
+VALUES('array');
+
+INSERT INTO Paradigma(Nome)
+VALUES('functional');
+
+INSERT INTO Paradigma(Nome)
+VALUES('structured');
+
+INSERT INTO Paradigma(Nome)
+VALUES('modular');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('APL',1966,null);
+
+INSERT INTO Paradigma(Nome)
+VALUES('imperative');
+
+INSERT INTO Paradigma(Nome)
+VALUES('unstructured');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Assembly',1949,'Assembly.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Bash',1989,'Bash.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('procedural');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('C',1972,'C.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('object-oriented');
+
+INSERT INTO Paradigma(Nome)
+VALUES('event-driven');
+
+INSERT INTO Paradigma(Nome)
+VALUES('task-driven');
+
+INSERT INTO Paradigma(Nome)
+VALUES('generic');
+
+INSERT INTO Paradigma(Nome)
+VALUES('reflective');
+
+INSERT INTO Paradigma(Nome)
+VALUES('concurrent');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('C#',2000,'C_Sharp.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('C++',1985,'Cpp.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('agent-oriented');
+
+INSERT INTO Paradigma(Nome)
+VALUES('logic');
+
+INSERT INTO Paradigma(Nome)
+VALUES('macro');
+
+INSERT INTO Paradigma(Nome)
+VALUES('pipeline');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Clojure',2007,'Clojure.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('COBOL',1959,'COBOL.jpg');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Crystal',2014,'Crystal.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Dart',2012,'Dart.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Delphi',null,'Delphi.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('distributed');
+
+INSERT INTO Paradigma(Nome)
+VALUES('process-oriented');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Elixir',2011,'Elixir.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Erlang',1986,'Erlang.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('metaprogramming');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('F#',2005,'F_Sharp.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Go',2009,'Go.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('scripting');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Groovy',2003,'Groovy.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Haskell',1990,'Haskell.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('HTML/CSS',null,'HTML_CSS.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('class-based');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Java',1995,'Java.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('JavaScript',1995,'JavaScript.jpg');
+
+INSERT INTO Paradigma(Nome)
+VALUES('meta');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Julia',2012,'Julia.png');
+
+INSERT INTO Paradigma(Nome)
+VALUES('block structured');
+
+INSERT INTO Paradigma(Nome)
+VALUES('declarative');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Kotlin',2011,'Kotlin.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('LISP',1958,'LISP.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Matlab',1970,'Matlab.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Node.js',2009,'Nodejs.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Objective-C',1984,null);
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Perl',1988,'Perl.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('PHP',1995,'PHP.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('PowerShell',2006,'PowerShell.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Python',1991,'Python.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('R',1993,'R.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Ruby',1995,'Ruby.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Rust',2010,'Rust.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Scala',2004,'Scala.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('SQL',1974,null);
+
+INSERT INTO Paradigma(Nome)
+VALUES('protocol-oriented');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('Swift',2014,'Swift.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('TypeScript',2012,'TypeScript.png');
+
+INSERT INTO Linguagem(Nome,AnoCriacao,Logo)
+VALUES ('VBA',1993,null);
+
