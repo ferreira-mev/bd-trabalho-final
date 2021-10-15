c471d0b2f89c19cc2c5e1e61aaccdfff0bb5c192
ferreira-mev <ferreira.mev@gmail.com>
2021-10-15 08:07:51 -0300

    Edita enum de tipo; comenta linhas relativas a pessoa na DDL

--- sql/ddl_brmodelo.sql
+++ sql/ddl_brmodelo.sql
@@ -1,26 +1,31 @@
 /*
+As linhas referentes a Pessoa e seus relacionamentos estao
+comentadas para testar a conversao das demais entidades de
+csv para SQL.
+
 TODO:
-- Definir enums (ou corrigir p/ o caso de multiplas respostas)
+- Definir enums ou mudar p/ set cf o caso
 - Adc restr integridade nao desejar SO?
+- Conferir gatilhos
 */
 
 CREATE DATABASE IF NOT EXISTS stackoverflow;
 
 USE stackoverflow;
 
-CREATE TABLE Pessoa (
-    Id INT PRIMARY KEY AUTO_INCREMENT,
-    Genero ENUM,
-    FaixaEtaria ENUM,
-    ExpTotal VARCHAR(255),
-    ExpProfissional VARCHAR(255),
-    EhProfissional ENUM,
-    TamEmpresa ENUM,
-    Cargo ENUM,
-    Salario INT,
-    NivelEduc ENUM,
-    Pais VARCHAR(255)
-);
+-- CREATE TABLE Pessoa (
+--     Id INT PRIMARY KEY AUTO_INCREMENT,
+--     Genero ENUM,
+--     FaixaEtaria ENUM,
+--     ExpTotal VARCHAR(255),
+--     ExpProfissional VARCHAR(255),
+--     EhProfissional ENUM,
+--     TamEmpresa ENUM,
+--     Cargo ENUM,
+--     Salario INT,
+--     NivelEduc ENUM,
+--     Pais VARCHAR(255)
+-- );
 
 CREATE TABLE Sgbd (
     Relacional BOOL,
@@ -40,7 +45,7 @@ CREATE TABLE Linguagem (
 );
 
 CREATE TABLE OutraTecnologia (
-    Tipo ENUM('Biblioteca', 'Cloud', 'Framework Web', 'IDE', 'SO', 'Outras Tecnologias') NOT NULL,
+    Tipo ENUM('Biblioteca', 'Nuvem', 'FrameworkWeb', 'EditorIde', 'So', 'Outro') NOT NULL,
     Logo VARCHAR(511),
     Nome VARCHAR(255),
     Id INT PRIMARY KEY AUTO_INCREMENT,
@@ -53,21 +58,21 @@ CREATE TABLE Paradigma (
     Nome VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE Usa (
-    fk_Pessoa_Id INT NOT NULL,
-    fk_Sgbd_Id INT,
-    fk_Linguagem_Id INT,
-    fk_OutraTecnologia_Id INT,
-    Id INT PRIMARY KEY AUTO_INCREMENT
-);
+-- CREATE TABLE Usa (
+--     fk_Pessoa_Id INT NOT NULL,
+--     fk_Sgbd_Id INT,
+--     fk_Linguagem_Id INT,
+--     fk_OutraTecnologia_Id INT,
+--     Id INT PRIMARY KEY AUTO_INCREMENT
+-- );
 
-CREATE TABLE Deseja (
-    fk_Pessoa_Id INT NOT NULL,
-    fk_Sgbd_Id INT,
-    fk_Linguagem_Id INT,
-    fk_OutraTecnologia_Id INT,
-    Id INT PRIMARY KEY AUTO_INCREMENT
-);
+-- CREATE TABLE Deseja (
+--     fk_Pessoa_Id INT NOT NULL,
+--     fk_Sgbd_Id INT,
+--     fk_Linguagem_Id INT,
+--     fk_OutraTecnologia_Id INT,
+--     Id INT PRIMARY KEY AUTO_INCREMENT
+-- );
 
 CREATE TABLE Associada (
     fk_Linguagem_Id INT NOT NULL,
@@ -82,50 +87,50 @@ CREATE TABLE Tem (
     fk_Paradigma_Id INT NOT NULL
 );
 
-ALTER TABLE Usa ADD CONSTRAINT FK_Usa_1
-    FOREIGN KEY (fk_Pessoa_Id)
-    REFERENCES Pessoa (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_1
+--     FOREIGN KEY (fk_Pessoa_Id)
+--     REFERENCES Pessoa (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Usa ADD CONSTRAINT FK_Usa_2
-    FOREIGN KEY (fk_Sgbd_Id)
-    REFERENCES Sgbd (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_2
+--     FOREIGN KEY (fk_Sgbd_Id)
+--     REFERENCES Sgbd (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Usa ADD CONSTRAINT FK_Usa_3
-    FOREIGN KEY (fk_Linguagem_Id)
-    REFERENCES Linguagem (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_3
+--     FOREIGN KEY (fk_Linguagem_Id)
+--     REFERENCES Linguagem (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Usa ADD CONSTRAINT FK_Usa_4
-    FOREIGN KEY (fk_OutraTecnologia_Id)
-    REFERENCES OutraTecnologia (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_4
+--     FOREIGN KEY (fk_OutraTecnologia_Id)
+--     REFERENCES OutraTecnologia (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_1
-    FOREIGN KEY (fk_Pessoa_Id)
-    REFERENCES Pessoa (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_1
+--     FOREIGN KEY (fk_Pessoa_Id)
+--     REFERENCES Pessoa (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_2
-    FOREIGN KEY (fk_Sgbd_Id)
-    REFERENCES Sgbd (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_2
+--     FOREIGN KEY (fk_Sgbd_Id)
+--     REFERENCES Sgbd (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_3
-    FOREIGN KEY (fk_Linguagem_Id)
-    REFERENCES Linguagem (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_3
+--     FOREIGN KEY (fk_Linguagem_Id)
+--     REFERENCES Linguagem (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_4
-    FOREIGN KEY (fk_OutraTecnologia_Id)
-    REFERENCES OutraTecnologia (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_4
+--     FOREIGN KEY (fk_OutraTecnologia_Id)
+--     REFERENCES OutraTecnologia (Id)
+--     ON DELETE SET NULL;
 
-ALTER TABLE Associada ADD CONSTRAINT FK_Associada_1
-    FOREIGN KEY (fk_Linguagem_Id)
-    REFERENCES Linguagem (Id)
-    ON DELETE SET NULL;
+-- ALTER TABLE Associada ADD CONSTRAINT FK_Associada_1
+--     FOREIGN KEY (fk_Linguagem_Id)
+--     REFERENCES Linguagem (Id)
+--     ON DELETE SET NULL;
 
 ALTER TABLE Associada ADD CONSTRAINT FK_Associada_2
     FOREIGN KEY (fk_OutraTecnologia_Id)
