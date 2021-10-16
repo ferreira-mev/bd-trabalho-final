02836177a62e9c7a638c92333d197153b9278f9c
ferreira-mev <ferreira.mev@gmail.com>
2021-10-16 08:58:36 -0300

    Outline building set of SET and ENUM values

--- sql/csv2sql_survey.py
+++ sql/csv2sql_survey.py
@@ -8,11 +8,15 @@ survey_csv = "/home/duda/Documents/edu/UFRJ1/5_2021-1/bdi/trabs/03_final/bd-trab
 dml_out = "sql/dml_Pessoa.sql"
 ddl_out = "sql/sets_enums.txt"  # será copiado para a DDL
 
-set_cols = ["Genero", "Cargo"]
-enum_cols = ["FaixaEtaria", "EhProfissional", "TamEmpresa", "NivelEduc"]
+# Para gerar os SETs e ENUMs a partir dos valores possíveis:
+set_enum_cols = {
+    "SET": ["Genero", "Cargo"],
+    "ENUM": ["FaixaEtaria", "EhProfissional", "TamEmpresa", "NivelEduc"]
+}
 
-set_values = set()
-enum_values = set()
+set_enum_values = {
+    k: {col: set() for col in v} for k, v in set_enum_cols.items()
+}
 
 with open(survey_csv, "r") as csv_file:
     reader = csv.DictReader(csv_file)
@@ -25,6 +29,19 @@ with open(survey_csv, "r") as csv_file:
         first_row = True  # hacky but meh
 
         for row in reader:
+            for mysql_type, cols_dict in set_enum_values.items():
+                for col, col_set in cols_dict.items():
+                    if col == "Genero":
+                        values = row[col].split(";")
+                    elif col == "Cargo":
+                        values = row[col].split("#")
+                    else:
+                        values = [row[col]]
+                    
+                    for value in values:
+                        if value != "null":
+                            col_set.add(value)
+
             if not first_row:
                 dml_file.write(",\n")
             
@@ -33,3 +50,8 @@ with open(survey_csv, "r") as csv_file:
             first_row = False
 
         dml_file.write(";")
+
+with open(ddl_out, "w") as ddl_file:
+    for mysql_type, cols_dict in set_enum_values.items():
+        for col, col_set in cols_dict.items():
+            ddl_file.write(col + str(col_set) + "\n")
