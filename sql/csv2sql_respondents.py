6c937fcae1119606e527fff7f06bfa56126b61c2
ferreira-mev <ferreira.mev@gmail.com>
2021-10-16 15:09:55 -0300

    Fix Pessoa DML generation (I think/hope)

---
+++ sql/csv2sql_respondents.py
@@ -0,0 +1,116 @@
+"""
+Script para gerar a DML em SQL a partir dos arquivos csv com
+as respostas da pesquisa.
+"""
+import csv
+
+survey_csv = "/home/duda/Documents/edu/UFRJ1/5_2021-1/bdi/trabs/03_final/bd-trabalho-final/datasets/pessoa.csv"
+dml_out = "sql/dml_Pessoa.sql"
+ddl_out = "sql/sets_enums.txt"  # será copiado para a DDL
+
+########
+# Deixei comentadas as linhas referentes à determinação dos
+# valores possíveis para definição dos SETs e ENUMs para 
+# para reduzir o tempo de execução, porque precisei alterar a
+# a geração da DML depois de a DDL já estar "pronta", 
+########
+
+# # Para gerar os SETs e ENUMs a partir dos valores possíveis:
+# set_enum_cols = {
+#     "SET": ["Genero", "Cargo"],
+#     "ENUM": ["FaixaEtaria", "EhProfissional", "TamEmpresa", "NivelEduc"]
+# }
+
+# set_enum_values = {
+#     k: {col: set() for col in v} for k, v in set_enum_cols.items()
+# }
+
+# Para não acrescentas aspas às colunas com valores numéricos:
+numeric_cols = {"Id", "ExpTotal", "ExpProfiss", "Salario"}
+
+def strip_quotes(string):
+    """
+    Retira todas as aspas (simples ou duplas) das strings.
+    """
+    return string.replace("'", "").replace('"', "")
+
+def swap_commas(string):
+    """
+    Substitui vírgulas em strings por sinais de mais.
+    """
+    return string.replace(",", "+")
+
+def single_quotes(string):
+    """
+    Uniformiza as aspas nas strings, inserindo aspas simples no início
+    e fim de todas elas, para inserção no SQL.
+    """
+    return "'" + strip_quotes(string) + "'"
+
+
+with open(survey_csv, "r") as csv_file:
+    reader = csv.DictReader(csv_file)
+    fields = reader.fieldnames
+    header = f"INSERT INTO Pessoa({', '.join(fields)}) VALUES\n"
+
+    with open(dml_out, "w") as dml_file:
+        dml_file.write(header)
+
+        first_row = True  # hacky but meh
+
+        for row in reader:
+            for col, entry in row.items():
+                # Separando os valores das colunas de tipo
+                # SET:
+                if col == "Genero":
+                    values = swap_commas(entry).split(";")
+                elif col == "Cargo":
+                    values = swap_commas(entry).split("#")
+                else:
+                    if "Exp" in col:
+                        # Há valores não numéricos nessas colunas
+                        # numéricas; estou substituindo por "Less than
+                        # 1 year" por 0 e "More than 50 years" por 51
+                        if "Less" in entry:
+                            entry = "0"
+                        elif "More" in entry:
+                            entry = "51"
+
+                    values = [entry]
+
+                values = [strip_quotes(v) for v in values]
+                # values = [swap_commas(v) for v in values]
+
+                row[col] = ",".join(values)
+                # Deixar a vírgula sem espaço depois; o MySQL considera
+                # trailing whitespace nos valores do SET e passa a 
+                # tratar como um valor inválido
+
+                # Adaptando as colunas com múltiplos valores à
+                # sintaxe do MySQL:
+                if col not in numeric_cols and row[col] != "null":
+                    row[col] = single_quotes(row[col])
+
+                # # Reunindo os diferentes valores possíveis para definir
+                # # SETs e ENUMs do MySQL:
+                # for mysql_type, cols_dict in set_enum_values.items():
+                #     if col in cols_dict.keys():
+                #         for value in values:
+                #             if value != "null":
+                #                 cols_dict[col].add(value)
+
+            if row and not first_row:
+                dml_file.write(",\n")
+            
+            dml_file.write(f"\t({', '.join(row.values())})")
+            
+            first_row = False
+
+        dml_file.write(";\n")
+
+# with open(ddl_out, "w") as ddl_file:
+#     for mysql_type, cols_dict in set_enum_values.items():
+#         for col, col_set in cols_dict.items():
+#             ddl_file.write(f"\t{col} {mysql_type}(")
+#             col_set = [single_quotes(s) for s in col_set]
+#             ddl_file.write(", ".join(col_set) + "),\n")
---
+++ sql/ddl.sql
@@ -0,0 +1,151 @@
+/*
+Obs: Note que os relacionamentos envolvendo Pessoa estao comentados
+por enquanto.
+
+TODO:
+- Adc restr integridade nao desejar SO?
+- Conferir gatilhos
+*/
+
+CREATE DATABASE IF NOT EXISTS stackoverflow;
+
+USE stackoverflow;
+
+/* As virgulas dos valores dos SETs foram trocadas por "+" por causa
+da sintaxe do MySQL (virgulas *dentro* das aspas separam valores na
+hora de inserir) */
+CREATE TABLE Pessoa (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Genero SET('Non-binary+ genderqueer+ or gender non-conforming', 'Man', 'Woman', 'Prefer not to say', 'Or+ in your own words:'),
+	Cargo SET('Marketing or sales professional', 'Engineer: site reliability', 'Educator', 'Other (please specify):', 'Designer', 'Developer: front-end', 'Developer: QA or test', 'Data or business analyst', 'Developer: game or graphics', 'Developer: back-end', 'Engineering manager', 'Student', 'Product manager', 'Developer: full-stack', 'Data scientist or machine learning specialist', 'Academic researcher', 'Developer: embedded applications or devices', 'System administrator', 'Engineer: data', 'Senior Executive (C-Suite: VP: etc.)', 'Developer: mobile', 'DevOps specialist', 'Scientist', 'Database administrator', 'Developer: desktop or enterprise applications'),
+	FaixaEtaria ENUM('35-44 years old', '45-54 years old', '18-24 years old', 'Under 18 years old', '25-34 years old', 'Prefer not to say', '55-64 years old', '65 years or older'),
+	EhProfissional ENUM('None of these', 'I am a developer by profession', 'I am not primarily a developer, but I write code sometimes as part of my work', 'I used to be a developer by profession, but no longer am', 'I code primarily as a hobby', 'I am a student who is learning to code'),
+	TamEmpresa ENUM('2 to 9 employees', 'I don’t know', '100 to 499 employees', 'Just me - I am a freelancer, sole proprietor, etc.', '10 to 19 employees', '5,000 to 9,999 employees', '10,000 or more employees', '1,000 to 4,999 employees', '500 to 999 employees', '20 to 99 employees'),
+	NivelEduc ENUM('Some college/university study without earning a degree', 'Something else', 'Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)', 'Bachelor’s degree (B.A., B.S., B.Eng., etc.)', 'Professional degree (JD, MD, etc.)', 'Associate degree (A.A., A.S., etc.)', 'Primary/elementary school', 'Master’s degree (M.A., M.S., M.Eng., MBA, etc.)', 'Other doctoral degree (Ph.D., Ed.D., etc.)'),
+    Salario INT,
+    Pais VARCHAR(255),
+    ExpTotal INT,
+    ExpProfiss INT
+);
+
+CREATE TABLE Sgbd (
+    Relacional BOOL,
+    Logo VARCHAR(511),
+    Nome VARCHAR(255),
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    AnoCriacao YEAR,
+    UNIQUE (Logo, Nome)
+);
+
+CREATE TABLE Linguagem (
+    Logo VARCHAR(511),
+    Nome VARCHAR(255) NOT NULL,
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    AnoCriacao YEAR,
+    UNIQUE (Logo, Nome)
+);
+
+CREATE TABLE OutraTecnologia (
+    Tipo ENUM('Nuvem', 'EditorIde', 'Biblioteca', 'So', 'Outro', 'FrameworkWeb') NOT NULL,
+    Logo VARCHAR(511),
+    Nome VARCHAR(255),
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    AnoCriacao YEAR,
+    UNIQUE (Logo, Nome)
+);
+
+CREATE TABLE Paradigma (
+    Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- CREATE TABLE Usa (
+--     fk_Pessoa_Id INT NOT NULL,
+--     fk_Sgbd_Id INT,
+--     fk_Linguagem_Id INT,
+--     fk_OutraTecnologia_Id INT,
+--     Id INT PRIMARY KEY AUTO_INCREMENT
+-- );
+
+-- CREATE TABLE Deseja (
+--     fk_Pessoa_Id INT NOT NULL,
+--     fk_Sgbd_Id INT,
+--     fk_Linguagem_Id INT,
+--     fk_OutraTecnologia_Id INT,
+--     Id INT PRIMARY KEY AUTO_INCREMENT
+-- );
+
+CREATE TABLE Associada (
+    fk_Linguagem_Id INT NOT NULL,
+    fk_OutraTecnologia_Id INT NOT NULL,
+    Dependente BOOL,
+    Id INT PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE Tem (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    fk_Linguagem_Id INT NOT NULL,
+    fk_Paradigma_Id INT NOT NULL
+);
+
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_1
+--     FOREIGN KEY (fk_Pessoa_Id)
+--     REFERENCES Pessoa (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_2
+--     FOREIGN KEY (fk_Sgbd_Id)
+--     REFERENCES Sgbd (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_3
+--     FOREIGN KEY (fk_Linguagem_Id)
+--     REFERENCES Linguagem (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Usa ADD CONSTRAINT FK_Usa_4
+--     FOREIGN KEY (fk_OutraTecnologia_Id)
+--     REFERENCES OutraTecnologia (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_1
+--     FOREIGN KEY (fk_Pessoa_Id)
+--     REFERENCES Pessoa (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_2
+--     FOREIGN KEY (fk_Sgbd_Id)
+--     REFERENCES Sgbd (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_3
+--     FOREIGN KEY (fk_Linguagem_Id)
+--     REFERENCES Linguagem (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Deseja ADD CONSTRAINT FK_Deseja_4
+--     FOREIGN KEY (fk_OutraTecnologia_Id)
+--     REFERENCES OutraTecnologia (Id)
+--     ON DELETE SET NULL;
+
+-- ALTER TABLE Associada ADD CONSTRAINT FK_Associada_1
+--     FOREIGN KEY (fk_Linguagem_Id)
+--     REFERENCES Linguagem (Id)
+--     ON DELETE SET NULL;
+
+ALTER TABLE Associada ADD CONSTRAINT FK_Associada_2
+    FOREIGN KEY (fk_OutraTecnologia_Id)
+    REFERENCES OutraTecnologia (Id)
+    ON DELETE SET NULL;
+
+/*
+ERROR 1830 (HY000): Column 'fk_OutraTecnologia_Id' cannot be NOT NULL: needed in a foreign key constraint 'FK_Associada_2' SET NULL
+*/
+
+ALTER TABLE Tem ADD CONSTRAINT FK_Tem_2
+    FOREIGN KEY (fk_Linguagem_Id)
+    REFERENCES Linguagem (Id);
+
+ALTER TABLE Tem ADD CONSTRAINT FK_Tem_3
+    FOREIGN KEY (fk_Paradigma_Id)
+    REFERENCES Paradigma (Id);
