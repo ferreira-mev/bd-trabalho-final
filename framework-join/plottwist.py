783934539faa5b248a7902ea6f946f403d845777
ferreira-mev <ferreira.mev@gmail.com>
2021-10-19 09:06:35 -0300

    Move % label generation to a separate function

--- framework-join/plottwist.py
+++ framework-join/plottwist.py
@@ -10,7 +10,7 @@ plot_ext = ".png"  # qual usar?
 
 def add_plot_path(plot_name):
     """
-    (srsly é uma linha)
+    (srsly, é uma linha, lê aí)
     """
     return plot_path + plot_name + plot_ext
 
@@ -31,17 +31,29 @@ def custom_save(plot_name, *args, **kwargs):
     return name_with_path
 
 
-def bake_pie(ord_dict):
+def add_perc_to_labels(ord_dict):
     """
-    Recebe um dicionário ou dicionário ordenado da forma {Nome: %}
-    (a percentagem é um foat entre 0 e 1).
+    Recebe um dicionário ordenado da forma {Nome: %} (a percentagem é
+    um float entre 0 e 1).
 
-    Gera e salva um gráfico de setores (pizza), retornando seu caminho.
+    Retorna uma lista de rótulos da forma "Nome: x.xx%". (É uma lista por conta da forma como o Matplotlib recebe esse argumento.)
     """
     labels = [
         f"{lang}: {(perc * 100):.2f}%"
         for lang, perc in ord_dict.items()
     ]
+
+    return labels
+
+
+def bake_pie(ord_dict):
+    """
+    Recebe um dicionário ordenado da forma {Nome: %} (a percentagem é 
+    um float entre 0 e 1).
+
+    Gera e salva um gráfico de setores (pizza), retornando seu caminho.
+    """
+    labels = add_perc_to_labels(ord_dict)
 
     plt.pie(
         np.array(list(ord_dict.values())),
