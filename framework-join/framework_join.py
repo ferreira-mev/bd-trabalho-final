4657c5e76f8601d26bae8162f4ca329ecb3a60ab
ferreira-mev <ferreira.mev@gmail.com>
2021-10-18 21:06:15 -0300

    Move pie baking to a function

--- framework-join/framework_join.py
+++ framework-join/framework_join.py
@@ -9,12 +9,29 @@ DEBUG = True
 ENV = 'development'
 app.config.from_object(__name__)
 
+
+# Gráfico de pizza:
 plot_path = "plots/"
 plot_ext = ".png"
 
 def plot_file(plot_name):
     return plot_path + plot_name + plot_ext
 
+def bake_pie(ord_dict):
+    """
+    Gera e salva o gráfico de pizza, retornando seu caminho.
+    """
+    plt.pie(
+        np.array(list(ord_dict.values())),
+        labels=list(ord_dict.keys())
+    )
+
+    plt.savefig(plot_file("pie"))
+
+    return plot_file("pie")
+
+
+# Aplicação web:
 @app.route("/")
 def placeholder():
     return "This page will be replaced by an actual homepage"
@@ -65,12 +82,7 @@ def frmwrk_ratio():
 
     # OrderedDict([('JavaScript', 0.6411618981606274), ('Python', 0.14162398757492908), ('PHP', 0.06618147278164992), ('C#', 0.06365281682005468), ('F#', 0.06365281682005468), ('Java', 0.058897146597866684), ('Ruby', 0.02848267806487222)])
     
-    plt.pie(
-        np.array(list(perc_lang_users.values())),
-        labels=list(perc_lang_users.keys())
-    )
-
-    plt.savefig(plot_file("pie"))
+    pie = bake_pie(perc_lang_users)
 
     rendered_template = render_template('frameworks.html.j2', cursor_from_python_code = cursor)
 
