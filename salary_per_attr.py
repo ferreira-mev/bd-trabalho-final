fe2c53494aa21cc2598d8a1c4375a51beab2a01b
ferreira-mev <ferreira.mev@gmail.com>
2021-10-19 22:55:02 -0300

    Another minor fix :P

--- salary_per_attr.py
+++ salary_per_attr.py
@@ -56,8 +56,6 @@ def frmwrk_ratio():
         plot=bar,
         alt_text="Gráfico de barras",
         page_title=f"Salário por {display_str(attr_name)}"
-        # TODO: "versão para impressão" dos nomes dos atributos
-        # (e tb de seus valores -- alguns são muito longos p/ gráficos)
     )
 
     cursor.close()
