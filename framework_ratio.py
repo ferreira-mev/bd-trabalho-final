a50cc9b753c5347334e38800fd07a22dc317649c
ferreira-mev <ferreira.mev@gmail.com>
2021-10-20 11:31:21 -0300

    Link all 3 pages to alt main
    (Apparently, it wasn't working before because of the word 'selector' :P)

--- framework_ratio.py
+++ framework_ratio.py
@@ -1,97 +1,18 @@
-from flask import Blueprint, Flask, session, url_for, render_template, request, redirect
+from flask import Flask, Blueprint, url_for, render_template, request, redirect
 import mysql.connector
 from collections import OrderedDict
 import plottwist, db_functions
 from ui_display import display_str, build_attr_dict
 
-from role_ratio import app_roles
-from salary_per_attr import app_salary
+# app_salary = Flask(__name__)
 
-app = Flask(__name__)
-app.register_blueprint(app_roles)
-app.register_blueprint(app_salary)
+# DEBUG = True
+# ENV = 'development'
+# app.config.from_object(__name__)
 
-DEBUG = True
-ENV = 'development'
-app.config.from_object(__name__)
+app_framework = Blueprint('app_framework',__name__)
 
-app.secret_key = "NEED_A_KEY_FOR_SESSION_VARIABLES"
-# (this is obviously unsafe, but I'm having enough trouble
-# as it is :P)
-
-@app.route("/")
-def placeholder():
-    # session["goal"] = "frameworks"
-    # session["goal"] = "cargos"
-    session["goal"] = "salarios"
-
-    if session["goal"] == "salarios":
-        action_url = "http://localhost:5000/salarios"
-    else:
-        action_url = "http://localhost:5000/selecionar-valor"
-    
-    rendered_template = render_template(
-        'attribute-selector.html.j2',
-        attr_list=["FaixaEtaria", "TamEmpresa", "NivelEduc", "Pais"], #"Genero", "Cargo"],
-        display_fn=display_str,
-        action_url=action_url
-    )
-
-    return rendered_template
-
-@app.route("/selecionar-valor", methods=['GET', 'POST'])
-def value_selector():
-    if request.method != 'POST':
-        return "Erro"
-    
-    cnx = db_functions.connect()
-
-    cursor = cnx.cursor(dictionary=True, buffered=True)
-    # Buffering: https://stackoverflow.com/a/33632767
-
-    attr_name = request.form.get("attr-select")
-    print(attr_name)
-
-    if attr_name not in {"Genero", "Cargo"}:
-        query = f"""
-            SELECT {attr_name} AS attr_value
-            FROM Pessoa
-            GROUP BY attr_value
-            ORDER BY attr_value;
-        """
-    else:
-        subquery = db_functions.subquery(attr_name)
-        query = f"""
-            SELECT S.{attr_name} AS attr_value
-            FROM ({subquery}) AS S
-            GROUP BY attr_value
-            ORDER BY attr_value;
-        """ # OK
-
-    cursor.execute(query)
-
-    values = []
-
-    for row in cursor:
-        if not row["attr_value"]:
-            row["attr_value"] = "null"
-        values.append(row["attr_value"])
-    print(values)
-    
-    rendered_template = render_template(
-        'value-selector.html.j2',
-        attr_name=attr_name,
-        attr_values=values,
-        display_fn=display_str,
-        action_url="http://localhost:5000/" + session["goal"]
-    )
-
-    cursor.close()
-    cnx.close()
-
-    return rendered_template
-
-@app.route("/frameworks", methods=['GET', 'POST'])
+@app_framework.route("/frameworks", methods=['GET', 'POST'])
 def frmwrk_ratio():
     attr_name, attr_value = request.form.get("value-select").split("#")
 
@@ -177,7 +98,3 @@ def frmwrk_ratio():
     cnx.close()
 
     return rendered_template
-
-
-if __name__ == "__main__":
-    app.run()
