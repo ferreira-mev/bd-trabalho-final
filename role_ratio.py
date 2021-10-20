c852180dc0f9568576674989441e846c97072ede
ferreira-mev <ferreira.mev@gmail.com>
2021-10-20 10:30:39 -0300

    Clean up comments

--- role_ratio.py
+++ role_ratio.py
@@ -1,8 +1,6 @@
 """
 Produz um gráfico de barras com a proporção de respondentes em cada 
 profissão.
-
-TODO: Comparar geral com valor de atributo?
 """
 from flask import Blueprint, session, Flask, url_for, render_template, request, redirect
 import mysql.connector
@@ -84,37 +82,6 @@ def role_per_attr():
             n_per_role[-1][k] = 100 * v / total
         
         bars.append(plottwist.plot_bar_abs(n_per_role[-1], unit="%"))
-
-    # cursor.execute(
-    #     f"""SELECT COUNT(Id) AS c FROM Pessoa
-    #     WHERE Pessoa.{attr_name} {db_functions.comp_or_null(attr_value)};"""
-    # )
-
-    # # verif se não é 0
-    
-    # total = cursor.fetchone()['c'] # 83439 s/ filtro
-
-    # query = f"""
-    #             SELECT COUNT(C.PId) AS role_count, Cargo.Nome as role_name FROM
-    #             (
-    #                 SELECT P.Id AS PId, TC.fk_Cargo_Id AS CId FROM
-    #                 Pessoa as P LEFT JOIN TemCargo AS TC
-    #                 ON P.Id = TC.fk_Pessoa_Id
-    #                 WHERE P.{attr_name} {db_functions.comp_or_null(attr_value)}
-    #             ) as C
-    #             JOIN Cargo ON Cargo.Id = C.CId
-    #             GROUP BY role_name
-    #             ORDER BY role_count DESC;
-    # """
-
-    # cursor.execute(query)
-
-    # n_per_role = db_functions.get_ord_dict(cursor, "role_name", "role_count", numeric=False)
-
-    # for k, v in n_per_role.items():
-    #     n_per_role[k] = 100 * v / total
-
-    # bar = plottwist.plot_bar_abs(n_per_role, unit="%")
 
     rendered_template = render_template(
         'tale-of-two-graphs.html.j2',
