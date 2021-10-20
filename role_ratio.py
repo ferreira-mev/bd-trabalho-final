b945be86c7bae155df4e3b9fbf5dd275fe54b664
ferreira-mev <ferreira.mev@gmail.com>
2021-10-20 10:29:43 -0300

    role_ratio works

--- role_ratio.py
+++ role_ratio.py
@@ -24,49 +24,106 @@ app_roles = Blueprint('app_role',__name__)
 
 @app_roles.route("/cargos", methods=['GET', 'POST'])
 def role_per_attr():
-    print(session["goal"])
-    attr_filter = "Pais"
-    filter_value = "Brazil"
+    if request.method != 'POST':
+        return "Erro"
+
+    attr_name, attr_value = request.form.get("value-select").split("#")
+    # attr_name = "Pais"
+    # attr_value = "Brazil"
+
     cnx = db_functions.connect()
     cursor = cnx.cursor(dictionary=True, buffered=True)
 
-    cursor.execute(
-        f"""SELECT COUNT(Id) AS c FROM Pessoa
-        WHERE Pessoa.{attr_filter} = '{filter_value}';"""
-    )
+    n_per_role, bars = [], []
 
-    # verif se não é 0
+    queries = [
+        [
+            f"""SELECT COUNT(Id) AS c FROM Pessoa;""",
+            
+            f"""
+            SELECT COUNT(C.PId) AS role_count, Cargo.Nome as role_name FROM
+            (
+                SELECT P.Id AS PId, TC.fk_Cargo_Id AS CId FROM
+                Pessoa as P LEFT JOIN TemCargo AS TC
+                ON P.Id = TC.fk_Pessoa_Id
+            ) as C
+            JOIN Cargo ON Cargo.Id = C.CId
+            GROUP BY role_name
+            ORDER BY role_count DESC;
+            """
+        ],
+        [
+            f"""SELECT COUNT(Id) AS c FROM Pessoa
+            WHERE Pessoa.{attr_name} {db_functions.comp_or_null(attr_value)};""",
+
+            f"""
+            SELECT COUNT(C.PId) AS role_count, Cargo.Nome as role_name FROM
+            (
+                SELECT P.Id AS PId, TC.fk_Cargo_Id AS CId FROM
+                Pessoa as P LEFT JOIN TemCargo AS TC
+                ON P.Id = TC.fk_Pessoa_Id
+                WHERE P.{attr_name} {db_functions.comp_or_null(attr_value)}
+            ) as C
+            JOIN Cargo ON Cargo.Id = C.CId
+            GROUP BY role_name
+            ORDER BY role_count DESC;
+            """
+        ]
+    ]
+
+    for pair in queries:
+        cursor.execute(pair[0])
+
+        total = cursor.fetchone()['c']  # pode ser 0?
+
+        cursor.execute(pair[1])
+
+        n_per_role.append(db_functions.get_ord_dict(cursor, "role_name", "role_count", numeric=False))
+
+        for k, v in n_per_role[-1].items():
+            n_per_role[-1][k] = 100 * v / total
+        
+        bars.append(plottwist.plot_bar_abs(n_per_role[-1], unit="%"))
+
+    # cursor.execute(
+    #     f"""SELECT COUNT(Id) AS c FROM Pessoa
+    #     WHERE Pessoa.{attr_name} {db_functions.comp_or_null(attr_value)};"""
+    # )
+
+    # # verif se não é 0
     
-    total = cursor.fetchone()['c'] # 83439 s/ filtro
+    # total = cursor.fetchone()['c'] # 83439 s/ filtro
 
-    query = f"""
-                SELECT COUNT(C.PId) AS role_count, Cargo.Nome as role_name FROM
-                (
-                    SELECT P.Id AS PId, TC.fk_Cargo_Id AS CId FROM
-                    Pessoa as P LEFT JOIN TemCargo AS TC
-                    ON P.Id = TC.fk_Pessoa_Id
-                    WHERE P.{attr_filter} = '{filter_value}'
-                ) as C
-                JOIN Cargo ON Cargo.Id = C.CId
-                GROUP BY role_name
-                ORDER BY role_count DESC;
-    """
+    # query = f"""
+    #             SELECT COUNT(C.PId) AS role_count, Cargo.Nome as role_name FROM
+    #             (
+    #                 SELECT P.Id AS PId, TC.fk_Cargo_Id AS CId FROM
+    #                 Pessoa as P LEFT JOIN TemCargo AS TC
+    #                 ON P.Id = TC.fk_Pessoa_Id
+    #                 WHERE P.{attr_name} {db_functions.comp_or_null(attr_value)}
+    #             ) as C
+    #             JOIN Cargo ON Cargo.Id = C.CId
+    #             GROUP BY role_name
+    #             ORDER BY role_count DESC;
+    # """
 
-    cursor.execute(query)
+    # cursor.execute(query)
 
-    n_per_role = db_functions.get_ord_dict(cursor, "role_name", "role_count", numeric=False)
+    # n_per_role = db_functions.get_ord_dict(cursor, "role_name", "role_count", numeric=False)
 
-    for k, v in n_per_role.items():
-        n_per_role[k] = 100 * v / total
+    # for k, v in n_per_role.items():
+    #     n_per_role[k] = 100 * v / total
 
-    bar = plottwist.plot_bar_abs(n_per_role, unit="%")
+    # bar = plottwist.plot_bar_abs(n_per_role, unit="%")
 
     rendered_template = render_template(
-        'plot-page.html.j2',
+        'tale-of-two-graphs.html.j2',
         cursor_from_python_code=cursor,
-        plot=bar,
-        alt_text="Gráfico de barras",
-        page_title="Proporção dos cargos"
+        plots=bars,
+        attr_name=display_str(attr_name),
+        value_name=display_str(attr_value)
+        # alt_text="Gráfico de barras",
+        # page_title="Proporção dos cargos"
     )
 
     cursor.close()
